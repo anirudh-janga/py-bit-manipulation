fd29e6fb43086974199e1ad0374dd3ab24d0d368
anirudh-janga <janirudh2005@gmail.com>
2015-07-23 18:45:02 -0700

    adding bit masking examples

---
+++ bit_masking/bit_masking.py
@@ -0,0 +1,24 @@
+
+class BitOperations(object):
+
+    def __init__(self, number):
+        self._number = number
+
+    def is_bit_on(self, bit_num):
+        return self._number & (1 << bit_num)
+
+    def set_bit(self, bit_num):
+        return self._number | (1 << bit_num)
+
+    def clear_bit(self, bit_num):
+        return self._number & ~(1 << bit_num)
+
+    def flip_bit(self, bit_num):
+        return self._number ^ (1 << bit_num)
+
+    def clear_last_set_bit(self):
+        return self._number & (self._number-1)
+
+
+
+
---
+++ bit_masking/test_bit_masking.py
@@ -0,0 +1,36 @@
+import unittest
+
+import bit_masking
+
+class TestBitManipulation(unittest.TestCase):
+
+    def setUp(self):
+        self.obj = bit_masking.BitOperations(0b11001100)
+
+    def test_is_bit_on(self):
+        self.assertTrue(self.obj.is_bit_on(3))
+
+    def test_set_bit(self):
+        num = self.obj.set_bit(0)
+        self.assertEqual(num, 0b11001101)
+
+    def test_clear_bit(self):
+        num = self.obj.clear_bit(2)
+        self.assertEqual(num, 0b11001000)
+
+    def test_flip_bit_case1(self):
+        num_1 = self.obj.flip_bit(0)
+        self.assertEqual(num_1, 0b11001101)
+
+    def test_flip_bit_case2(self):
+        num_2 = self.obj.flip_bit(7)
+        self.assertEqual(num_2, 0b01001100)
+
+    def test_clear_last_set_bit(self):
+        num = self.obj.clear_last_set_bit()
+        self.assertEqual(num, 0b11001000)
+
+if __name__ == "__main__":
+    unittest.main()
+
+
